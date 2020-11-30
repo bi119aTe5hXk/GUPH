159ba7467908c1de13812c5254185ec20cdc5d07
bi119aTe5hXk <billgateshxk@gmail.com>
2020-11-30 20:38:07 +0900

    Initial Commit

---
+++ GUPHMac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GUPHMac
+//
+//  Created by billgateshxk on 2020/11/30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
